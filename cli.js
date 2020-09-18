5dea31d4860ae14fb46c36ac28c81ef052812fe6
Anastasiia Byvsheva <gnomus042@gmail.com>
2020-09-18 16:43:11 +0300

    SHACL added to cli

--- cli.js
+++ cli.js
@@ -32,45 +32,80 @@ function cliFormatToN3(val) {
     }
 }
 
+function writeResult(val, args) {
+    if (!args.output) {
+        console.log(result);
+    } else {
+        fs.writeFileSync(args.output, val);
+    }
+}
+
 function writeQuads(quads, output, format) {
     let writer = new n3.Writer({format: cliFormatToN3(format)});
     quads.forEach(quad => writer.addQuad(quad.subject, quad.predicate, quad.object));
     writer.end((error, result) => {
         if (error) throw new CliError(error);
-        if (!output) {
-            console.log(result);
-        } else {
-            fs.writeFileSync(output, result);
-        }
+        writeResult(result, args);
     });
 }
 
-let args = minimist(process.argv.slice(2));
-if (!args.input) throw new CliError('No input file path specified');
-if (args.output && !fs.existsSync(path.parse(args.output).dir)) throw new CliError(`Output directory \'${path.parse(args.output).dir}\' doesn\'t exist`);
-let data = fs.readFileSync(args.input).toString();
-if (args.parse) {
-    utils.inputToQuads(data, utils.randomUrl(), context)
-        .then(quads => writeQuads(quads, args.output, args.format))
-        .catch(err => {
-            throw new CliError(err.message)
-        });
-} else if (args.validate) {
-    if (!args.shex) {
-        throw new CliError('No ShEx shapes file specified');
-    }
+async function validateShEx(data, args) {
     args.service = args.service || '';
-    utils.loadData(args.shex)
-        .then(shapes => {
-            let validator = new ShexValidator(context, shapes);
-            validator.validate(data, args.service)
-                .then(report => {
-                    if (!args.output) console.log(report.failures);
-                    else fs.writeFileSync(args.output, report.failures);
-                });
-
-        })
-        .catch(err => {
-            throw new CliError(err.message)
-        });
+    let shapes = await utils.loadData(args.shex);
+    let validator = new ShexValidator(context, shapes);
+    let report = await validator.validate(data, args.service)
+    writeResult(JSON.stringify(report.failures, undefined, 2), args);
 }
+
+async function validateShacl(data, args) {
+    let shapes = await utils.loadData(args.shacl);
+    let subclasses, annotations;
+    if (args.subclasses) {
+        subclasses = await utils.loadData(args.subclasses);
+    }
+    if (args.annotations) {
+        annotations = JSON.parse(await utils.loadData(args.annotations));
+    }
+    let validator = new ShaclValidator(shapes, {
+        subclasses: subclasses,
+        context: context,
+        annotations: annotations
+    });
+    let report = validator.validate(data);
+    writeResult(JSON.stringify(report.failures, undefined, 2), args);
+}
+
+/**
+ * @param {args} args
+ * @return {Promise<void>}
+ */
+async function main(args) {
+    if (!args.input) throw new CliError('No input file path specified');
+    if (args.output && !fs.existsSync(path.parse(args.output).dir)) throw new CliError(`Output directory \'${path.parse(args.output).dir}\' doesn\'t exist`);
+    let data = fs.readFileSync(args.input).toString();
+    if (args.parse) {
+        let quads = await utils.inputToQuads(data, utils.randomUrl(), context);
+        writeQuads(quads, args.output, args.format);
+    } else if (args.validate) {
+        if (args.shex && args.shacl) throw new CliError('Validation is possible for ShEx or SHACL, but not for both');
+        else if (args.shex) await validateShEx(data, args);
+        else if (args.shacl) await validateShacl(data, args);
+        else throw new CliError('No shapes provided for validation');
+    }
+}
+
+/**
+ * @typedef {{
+ *     input: string
+ *     output: string|undefined
+ *     parse: boolean|undefined
+ *     format: string|undefined
+ *     validate: boolean|undefined
+ *     shex: string|undefined
+ *     shacl: string|undefined
+ * }} args - possible options for args
+ */
+
+let args = minimist(process.argv.slice(2));
+main(args);
+
